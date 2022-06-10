b9f741b0bacd179722208236a9e5ee76b4833573
Brayancito2503 <brayan.potosme03@gmail.com>
2022-06-09 21:58:44 -0600

    Y SERA LA ULTIMAAA NOCHEE

--- PesoApp/app/src/main/java/com/example/pesoapp/View/Fragment_Calculadora.kt
+++ PesoApp/app/src/main/java/com/example/pesoapp/View/Fragment_Calculadora.kt
@@ -36,27 +36,27 @@ class Fragment_Calculadora : Fragment() {
         f2 = 45f
     }
 
-    fun btnConfirmarOnClick(v: View) {
-        val lblPi = findViewById<View>(R.id.pilbl) as TextView
-        val lblInc = findViewById<View>(R.id.bmibl) as TextView
-        val txtPeso = findViewById<View>(R.id.pesoTxt) as TextView
-        val txtAltura = findViewById<View>(R.id.alturaTxt) as TextView
-        val peso = txtPeso.text.toString().toFloat()
-        val altura = txtAltura.text.toString().toFloat()
-        val resultado = peso / (altura/100).pow(2)
-        val pi: Double = ((((altura - 152.4) / 2.54 )* f1) + f2)
-        if (resultado < 18) {
-            lblInc.text = "Debajo de lo normal $resultado"
-        } else if (resultado >= 18.1 && resultado <= 24.9) {
-            lblInc.text = "Peso Normal $resultado"
-        } else if (resultado >= 25 && resultado <= 29.9) {
-            lblInc.text = "Sobre Peso $resultado"
-        } else if (resultado >= 30 && resultado <= 34.9) {
-            lblInc.text = "Obesidad tipo I $resultado"
-        } else if (resultado > 35) {
-            lblInc.text = "Obesidad tipo II $resultado"
-        }
-        lblPi.text = "$pi"
-    }
+//    fun btnConfirmarOnClick(v: View) {
+//        val lblPi = findViewById<View>(R.id.pilbl) as TextView
+//        val lblInc = findViewById<View>(R.id.bmibl) as TextView
+//        val txtPeso = findViewById<View>(R.id.pesoTxt) as TextView
+//        val txtAltura = findViewById<View>(R.id.alturaTxt) as TextView
+//        val peso = txtPeso.text.toString().toFloat()
+//        val altura = txtAltura.text.toString().toFloat()
+//        val resultado = peso / (altura/100).pow(2)
+//        val pi: Double = ((((altura - 152.4) / 2.54 )* f1) + f2)
+//        if (resultado < 18) {
+//            lblInc.text = "Debajo de lo normal $resultado"
+//        } else if (resultado >= 18.1 && resultado <= 24.9) {
+//            lblInc.text = "Peso Normal $resultado"
+//        } else if (resultado >= 25 && resultado <= 29.9) {
+//            lblInc.text = "Sobre Peso $resultado"
+//        } else if (resultado >= 30 && resultado <= 34.9) {
+//            lblInc.text = "Obesidad tipo I $resultado"
+//        } else if (resultado > 35) {
+//            lblInc.text = "Obesidad tipo II $resultado"
+//        }
+//        lblPi.text = "$pi"
+//    }
 
 }
